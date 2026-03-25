8052156d33a1f56d9cfab0d840a5b7871f3670bf
vaishnavi-gor <vrgor204@gmail.com>
2026-03-25 19:56:23 +0100

    Add files via upload

---
+++ python-practice.py
@@ -0,0 +1,11 @@
+print("What drink do you want?")
+drink=input()
+if drink =="Coffee":
+    print("Here is your cappuccino!")
+elif drink =="Tea":
+    print("Here is your green tea!")
+else:
+    print("Sorry we don't have that drink.")
+print("End")    
+    
+
