55f744848ae970a6fe786d19298d7ba084a00de5
vaishnavi-gor <vrgor204@gmail.com>
2026-03-25 20:36:32 +0100

    Add files via upload

---
+++ python-practice.py.4.py
@@ -0,0 +1,14 @@
+print("What food do you want ?")
+food = input()
+print("What drink do you want ?")
+drink = input()
+if food == "Pizza" and drink == "Coke":
+    print("Perfect combo!")
+else:
+    print("Enjoy your meal!")
+    
+
+
+      
+     
+     
