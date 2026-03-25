0554264d622d8449e914493213bcdc3942ba89bd
vaishnavi-gor <vrgor204@gmail.com>
2026-03-25 20:15:04 +0100

    Add files via upload

---
+++ python-practice.py.3.py
@@ -0,0 +1,9 @@
+print("Which fruits do you like?")
+fruit=input()
+if fruit == "Apple":
+    print("Apples are healthy!")
+elif fruit == "Banana":
+    print("Bananas give energy!")
+else:     
+    print("Nice choice!")
+    
